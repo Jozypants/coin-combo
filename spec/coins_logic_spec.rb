bb039956ee95186ec02a9e54ba8db7f67086266a
Allison Mackey <a.mackey693@gmail.com>
2020-05-07 14:31:54 -0700

    write and test on spec #2 for nickels, expect test to fail

--- spec/coins_logic_spec.rb
+++ spec/coins_logic_spec.rb
@@ -2,10 +2,15 @@ require('rspec')
 require('coins_logic')
 
 describe('Change#currency') do 
-  it("converts dollar amount to cents by multiplying input by 100") do
+  it("converts dollar amount to cents by multiplying input by 100 and finding amount in pennies") do
     change = Change.new()
     expect(change.currency(".04")).to(eq([4]))
   end
+  it("converts dollar amount to cents by multiplying input by 100 and finds amount in nickels & in pennies") do
+    change = Change.new()
+    expect(change.currency(".09")).to(eq([1, 4]))
+  end
+
 end
 # it("divides converted amoutn by the next largest coin amount 10 - dimes and gives the remainder") do 
 #   change = Change.new()
