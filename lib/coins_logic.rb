dc85d7dd198c193dda34040b9763ede14f4eb309
Allison Mackey <a.mackey693@gmail.com>
2020-05-07 09:32:56 -0700

    set up currency test to fail.

--- lib/coins_logic.rb
+++ lib/coins_logic.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
 require('pry') 
+
+class Change 
+  def initialze(user_input, )
+    @user_input = user_input
+end 
--- spec/coins_logic_spec.rb
+++ spec/coins_logic_spec.rb
@@ -1,2 +1,8 @@
 require('rspec')
 require('coins_logic')
+
+describe('#currency') do 
+  if("converts dollar amount to cents by bultiplying input by 100") do
+    expect(currency(10.99)).to(eq(1099))
+  end
+end
